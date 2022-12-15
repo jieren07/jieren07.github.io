7aea79fd7653de841bec4c71ca6e9eaa98ee0518
Jieren <64475948+jieren0721@users.noreply.github.com>
2022-12-16 00:01:59 +0800

    Update html_template.js

--- controller/html_template.js
+++ controller/html_template.js
@@ -5,9 +5,12 @@ class Footer extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
+    <p>Jieren0721, Copyright &copy 2020 | Powered by <a href="https://pages.github.com/">Github Pages.</a></p>
+		<p>Site was built using HTML5, CSS, Javascript and Atom.</p>
     <a href='bit.ly/3FUjQrL'><img id='donate' src='https://raw.githubusercontent.com/bnkcollections/bnkcollections.github.io/master/img/donate.jpg' type='button'/></a>
+    <p class="conUs" onclick="navClick('contact')">Contact Us</p>
     `;
   }
 }
 
-customElements.define('footer-component', Footer);
+  customElements.define('footer-donate', Footer);
