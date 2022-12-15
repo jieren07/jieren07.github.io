0d213f21f93118f5ebd2309a5da39c809cb78e66
Jieren <64475948+jieren0721@users.noreply.github.com>
2022-12-16 01:01:54 +0800

    update link

--- controller/html_template.js
+++ controller/html_template.js
@@ -5,10 +5,10 @@ class Footer extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-    <p>Jieren0721, Copyright &copy 2020 | Powered by <a href="https://pages.github.com/">Github Pages.</a></p>
-		<p>Site was built using HTML5, CSS, Javascript and Atom.</p>
-    <a href='bit.ly/3FUjQrL'><img id='donate' src='https://raw.githubusercontent.com/bnkcollections/bnkcollections.github.io/master/img/donate.jpg' type='button'/></a>
-    <p class="conUs" onclick="navClick('contact')">Contact Us</p>
+    	<p>Jieren0721, Copyright &copy 2020 | Powered by <a href="https://pages.github.com/">Github Pages.</a></p>
+	<p>Site was built using HTML5, CSS, Javascript and Atom.</p>
+    	<a href="https://donate.stripe.com/3cs4gibQv07WcKI3cc"  target='_blank'><img onvlick="navDonate()" id='donate' src='https://raw.githubusercontent.com/bnkcollections/bnkcollections.github.io/master/img/donate.jpg' type='button'/></a>
+    	<p class="conUs" onclick="navClick('contact')">Contact Us</p>
     `;
   }
 }
